a5e1002d7366ac89a76d08be8009b1d9dbee4725
ppidcard <tvxqlikei@live.cn>
2021-04-18 02:09:52 +1000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const writeCardsNames = ['Lisa', 'Kaitlin', 'Jan']
+const writeCards = function(arr, adj){
+    let message = [];
+    for (let i=0; i<arr.length; i++){
+    message.push(`Thank you, ${arr[i]}, for the wonderful ${adj} gift!`);
+    }
+    return message;
+}
+
+const countDown = function(num){
+    for(let i=num; i>=0; i--){
+    console.log(i);
+    }
+}
+
